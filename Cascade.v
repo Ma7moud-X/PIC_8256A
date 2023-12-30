492c6bc01687cfb7383d0b5620491e644ad25d84
Mohamed Rezk <mohrizq895@gmail.com>
2023-12-30 19:13:09 +0200

    fixing inout port

--- Cascade.v
+++ Cascade.v
@@ -1,14 +1,13 @@
 																				
 module Cascade_Buffer (	
 	
-	input wire [2:0] CAS_IN,  //IN : Slave, receive the ID to compare
-	output reg [2:0] CAS_OUT, //OUT : Master, send the ID to all slaves
-	
+	inout wire [2:0] CAS, 
+
 	input wire SP_EN, // from and to CPU, OUT : Buffered (not supported), IN : Cascade, SP = 1 for master, SP = 0 for slave
-  input wire Cascade_reset, // From control for the slave mode address to set to priority 7 //its address (also known as the interrupt vector) is set to 7
+    input wire Cascade_reset, // From control for the slave mode address to set to priority 7 //its address (also known as the interrupt vector) is set to 7
 	input wire SNGL, // from control : if 1 sngl if 0 Cascade
 	input wire [2:0]ISR, // from ISR to knew which one i'm working on now to send to slaves
-	
+	input wire INTA_2,
 	output reg Master_Slave, // to control // 1 master, 0 slave
 	output reg [2:0] ID // to control to copmare IDs when slave 
 	
@@ -27,14 +26,13 @@ module Cascade_Buffer (
 	
 	always @* begin
 	    if(!Cascade_reset)begin
-			if(Master_Slave) begin // Master
-				CAS_OUT = ISR;
-			end else begin         // SLave
-				ID = CAS_IN;
+			if(!Master_Slave) begin // Slave
+				ID = CAS;
 			end
 		end
 	end
 	
+	assign CAS = !Cascade_reset && Master_Slave && !SNGL && INTA_2  ? ISR : 3'bz ;
 
 endmodule
 
@@ -46,30 +44,34 @@ module Cascade_Buffer_tb();
 
 	// input
 	reg Cascade_reset;
-	reg [2:0] CAS_IN;
 	reg [2:0] ISR;
 	reg SP_EN;
 	reg SNGL;
+  reg INTA_2;
 
+	// inout
+	wire [2:0] CAS;
+	
 	// output
-	wire [2:0] CAS_OUT;
 	wire [2:0] ID;
 	wire Master_Slave;
 	
 Cascade_Buffer test (
 		// input
 		.Cascade_reset(Cascade_reset),
-		.CAS_IN(CAS_IN),
 		.ISR(ISR),
 		.SP_EN(SP_EN),
 		.SNGL(SNGL),
+		.INTA_2(INTA_2),
+		// inout
+		.CAS(CAS),
 		// output
-		.CAS_OUT(CAS_OUT),
 		.ID(ID),
 		.Master_Slave(Master_Slave)
 );
 
-
+	reg [2:0] t;
+	assign CAS = !Cascade_reset && !Master_Slave && !SNGL ? t : 3'bz ;
 
 initial begin
     Cascade_reset = 1'b1;
@@ -89,15 +91,15 @@ initial begin
    
 	// Cascade : Slave
 	SP_EN = 1'b0;
-	CAS_IN = 4;
+	t = 4;
     #1000;
        
     $finish;
 end
 
 initial begin
-    $monitor("Time: %t, Cascade_reset: %b, SNGL: %b, SP_EN: %b,   ISR: %b %b %b,   CAS_IN: %b %b %b   OUTPUT   CAS_OUT: %b %b %b, Master_Slave: %b,   ID:  %b %b %b",
-    $time,Cascade_reset,SNGL,SP_EN,ISR[2],ISR[1],ISR[0],CAS_IN[2],CAS_IN[1],CAS_IN[0],CAS_OUT[2],CAS_OUT[1],CAS_OUT[0],
+    $monitor("Time: %t, Cascade_reset: %b, SNGL: %b, SP_EN: %b,   ISR: %b %b %b,  INOUT  CAS: %b %b %b   OUTPUT  Master_Slave: %b,   ID:  %b %b %b",
+    $time,Cascade_reset,SNGL,SP_EN,ISR[2],ISR[1],ISR[0],CAS[2],CAS[1],CAS[0],
     Master_Slave,ID[2],ID[1],ID[0]);
 
     $timeformat(-9, 1, " ns", 10);
@@ -105,6 +107,9 @@ end
 
 endmodule	
 	
+
+
+
 
 
 
